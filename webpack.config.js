1ff4422b0d59baef0336b9cba0a9f6da8107b13f
Maciej Kucharski <maciek.zir+github@gmail.com>
2015-03-25 22:55:29 +0100

    [master][setup] everything we need :)

--- webpack.config.js
+++ webpack.config.js
@@ -1,36 +1,11 @@
 module.exports = {
-    entry: './main.js',
+    entry: './app/main.js',
     output: {
-        filename: 'bundle.js'
+        filename: './app/bundle.js'
     },
     module: {
         loaders: [
             { test: /\.js$/, exclude: /node_modules/, loader: 'babel-loader'}
-        ],
-        preLoaders: [
-            {
-                test: /\.js$/,
-                exclude: /node_modules/,
-                loader: "jshint-loader"
-            }
         ]
-    },
-    jshint: {
-        camelcase: true,
-        emitErrors: true,
-        eqeqeq: true,
-        forin: true,
-        esnext: true,
-        latedef: 'nofunc',
-        maxcomplexity: 4,
-        maxdepth: 3,
-        maxparams: 3,
-        maxstatements: 5,
-        undef: true,
-        unused: true,
-        curly: true,
-        strict: true,
-        browser: true,
-        bitwise: true
     }
 };
